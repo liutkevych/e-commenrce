fb758b2e0b1ec9563fe41468a4d5255915accf88
liutkevych <oli@keepit.com>
2019-10-24 18:20:49 +0300

    Added custom input, button and form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Switch, Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInPage from './pages/signin/signin.component';
 
 import './App.css';
 
@@ -16,6 +17,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInPage} />
         </Switch>
       </div>
     );
---
+++ src/pages/signin/signin.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in.component';
+import './signin.style.scss';
+
+const SingInPage = () => (
+    <SignIn className='sign-in-and-sign-up'/>
+);
+
+export default SingInPage;
