e2bc77dc857d36b40bf35f118e909d9b9fca4e79
liutkevych <oli@keepit.com>
2019-10-25 17:07:14 +0300

    Add firebase and auth

--- src/App.js
+++ src/App.js
@@ -5,10 +5,32 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInPage from './pages/signin/signin.component';
+import { auth } from './firebase/firebase.utils';
 
 import './App.css';
 
 class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      currenntUser: null
+    }
+  }
+
+  unsubcribeFromAuth = null;
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user});
+
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubcribeFromAuth();
+  }
 
   render(){
     return (
